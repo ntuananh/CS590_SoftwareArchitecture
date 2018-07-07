6cf0d5b874489c4b8475d683dfa78fbef3402005
nanh <nta.hcmut@gmail.com>
2018-07-07 09:29:36 -0500

    Lab8

---
+++ Lab/Lab8/L8ConfigClientB/src/main/java/config/L8ConfigClientApplication.java
@@ -0,0 +1,12 @@
+package config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L8ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L8ConfigClientApplication.class, args);
+	}
+}
